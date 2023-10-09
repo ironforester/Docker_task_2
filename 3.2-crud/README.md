5d42cbe9502ab245081f74ad78da24975eec83e4
Belyakov <sano.belyakov@mail.ru>
2023-10-09 21:08:12 +0300

    initial commit

---
+++ 3.2-crud/README.md
@@ -0,0 +1,5 @@
+# Команды для запуска контейнера
+
+создание образа: docker build . --tag=docker_task_2
+
+запуск контейнера:  docker run -d -p 8000:8000 --name=docker_task2 docker_task_2
---
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -0,0 +1,25 @@
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['products__title', 'products__description']
+
+
+@api_view(['GET'])
+def just_view(request):
+    return Response('Another text')
